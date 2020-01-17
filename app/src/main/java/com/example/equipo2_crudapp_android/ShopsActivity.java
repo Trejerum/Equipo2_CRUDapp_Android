4b3909ac5f2acdaa76e3de683b911d98bebeb486
Diego Corral <dicorral28@gmail.com>
2020-01-17 12:50:17 +0100

    user view edited, shops view added

--- app/src/main/java/com/example/equipo2_crudapp_android/ShopsActivity.java
+++ app/src/main/java/com/example/equipo2_crudapp_android/ShopsActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UserViewActivity extends AppCompatActivity {
+public class ShopsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_view);
+        setContentView(R.layout.activity_shops);
     }
 }
---
+++ app/src/main/java/com/example/equipo2_crudapp_android/UserActivity.java
@@ -0,0 +1,14 @@
+package com.example.equipo2_crudapp_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
