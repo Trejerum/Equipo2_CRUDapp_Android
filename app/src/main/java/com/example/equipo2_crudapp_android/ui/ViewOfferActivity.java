5cfc0c85c6e66b737363f0e55627614655de5356
iker lopez carrillo <iker98lopez@gmail.com>
2020-01-31 10:36:39 +0100

    Started codind ViewOffer.

---
+++ app/src/main/java/com/example/equipo2_crudapp_android/ui/ViewOfferActivity.java
@@ -0,0 +1,16 @@
+package com.example.equipo2_crudapp_android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.equipo2_crudapp_android.R;
+
+public class ViewOfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_offer);
+    }
+}
